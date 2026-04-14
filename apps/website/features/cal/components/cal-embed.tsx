c7c2a7a5aea9347274af79adbf8bb057c8e37959
SriSomanaath <govindalapudisrinath@gmail.com>
2026-04-14 20:33:39 +0530

    chore(cal): point booking embed at ruixen-ui/30min

--- apps/website/features/cal/components/cal-embed.tsx
+++ apps/website/features/cal/components/cal-embed.tsx
@@ -5,7 +5,7 @@ import Cal from '@calcom/embed-react';
 export default function CalEmbed() {
   return (
     <div className="cal-embed">
-      <Cal namespace="30min" calLink="miravasquez/30min" />
+      <Cal namespace="30min" calLink="ruixen-ui/30min" />
     </div>
   );
 }
